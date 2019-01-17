a6ef1fd33b480eef3c728630b33ed186905c24a0
saxsir <saxsir.256+github@gmail.com>
2019-01-17 19:56:42 +0900

    とりあえず見た目だけupload

--- server.go
+++ server.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	e.Static("/static", "static")
-	e.File("/", "static/index.html")
+	e.Static("/", "static")
+
+	e.POST("/upload", func(c echo.Context) error {
+		file, err := c.FormFile("file")
+		if err != nil {
+			return err
+		}
+
+		return c.HTML(http.StatusOK, fmt.Sprintf("<p>File %s uploaded successfully.</p>", file.Filename))
+	})
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
